6b83cb1e11916cf5fa89b85103e00adb6c9ace16
Michael Rodenbuecher <ventar@outlook.com>
2019-09-04 06:56:53 +0200

    Presence Board Hakuna Connection

--- Common Libraries/lib/AsyncNeoPixel/AnimationStatic.cpp
+++ Common Libraries/lib/AsyncNeoPixel/AnimationStatic.cpp
@@ -5,9 +5,11 @@ namespace leds {
 void AnimationStatic::reset() {
   setAll(0, 0, 0);
 
-  if (data->colors[0] == 0) {
-    setColorListFromString("000044");
-  }
+  /*
+    if (data->colors[0] == 0) {
+      setColorListFromString("000044");
+    }
+    */
 
   for (uint16_t i = 0; i < strip->numPixels(); i++) {
     strip->setPixelColor(i, data->colors[i]);
--- PresenceBoard/src/main.cpp
+++ PresenceBoard/src/main.cpp
@@ -1,40 +1,76 @@
 #include <Arduino.h>
+#include <AsyncNeoPixel.h>
 #include <CircuitClient.h>
 #include <CommonUtilities.h>
+#include <WebServer.h>
 #include <vector>
 
 struct NeoCube {
-  NeoCube(uint16_t ledStart, uint16_t ledEnd, String userId) {
-    this->ledEnd = ledEnd;
-    this->ledStart = ledStart;
-    this->userId = userId;
+  NeoCube(String userId) { this->userId = userId; }
 
-  }
-
-  uint16_t ledStart;
-  uint16_t ledEnd;
   String userId;
   String color = "222222";
-
 };
 
-const String WIFI_NAME = "MRO Finger Weg";
-const String WIFI_PASSWORT = "7b559b98adc2";
+// const String WIFI_NAME = "MRO Finger Weg";
+// const String WIFI_PASSWORT = "7b559b98adc2";
 
-// const String WIFI_NAME = "Destiny_EXT";
-// const String WIFI_PASSWORT = "8882941015907883";
+const String WIFI_NAME = "Destiny_EXT";
+const String WIFI_PASSWORT = "8882941015907883";
 
 const String CIRCUIT_SYSTEM = "hakuna.circuitsandbox.net";
 const String CIRCUIT_USER = "michael.rodenbuecher@spaces.com";
 const String CIRCUIT_PWD = "sdf56JKL!";
 
-const NeoCube NeoCube1(0,8, "69ac9075-0eae-4c7a-a7ec-911ecc9bbb58"); // Mischa van Oijen
-const NeoCube NeoCube2(9,17, "55749dad-e394-4349-b4c6-ff94c10c0f00"); // Michael Schuergers
-const NeoCube NeoCube3(18,26, "c822303d-262c-4340-8f62-c94b0f757eb8"); // Alper Teke
-const NeoCube NeoCube4(27,35, "0b2aa684-fba1-4b73-ab31-64902076eed1"); // Rodrigo Pastro
+NeoCube* c1 = new NeoCube("69ac9075-0eae-4c7a-a7ec-911ecc9bbb58");  // Mischa van Oijen
+NeoCube* c2 = new NeoCube("55749dad-e394-4349-b4c6-ff94c10c0f00");  // Michael Schuergers
+NeoCube* c3 = new NeoCube("c822303d-262c-4340-8f62-c94b0f757eb8");  // Alper Teke
+NeoCube* c4 = new NeoCube("0b2aa684-fba1-4b73-ab31-64902076eed1");  // Rodrigo Pastro
 
+leds::AsyncNeoPixel strip(40, D4, NEO_GRB + NEO_KHZ800);
+// webserver::WebServer server(80);
 Circuit::CircuitClient* circuit;
+
 std::vector<String> userIds;
+std::vector<NeoCube*> neoCubes;
+
+/**
+ * Set the color of a cube based on the presence state.
+ */
+void updateNeoCube(NeoCube* cube, Circuit::PresenceState state) {
+  if (state.presenceState == "DND") {
+    Serial.printf("Change color of user ::= [%s] to ::= [FF0000]\n", state.userId.c_str());
+    cube->color = "ff0000";
+  } else if (state.presenceState == "AVAILABLE") {
+    Serial.printf("Change color of user ::= [%s] to ::= [00FF00]\n", state.userId.c_str());
+    cube->color = "00ff00";
+  } else if (state.presenceState == "BUSY") {
+    Serial.printf("Change color of user ::= [%s] to ::= [0000FF]\n", state.userId.c_str());
+    cube->color = "0000ff";
+  } else if (state.presenceState == "AWAY") {
+    Serial.printf("Change color of user ::= [%s] to ::= [0000FF]\n", state.userId.c_str());
+    cube->color = "FFFF00";
+  } else {
+    Serial.printf("Change color of user ::= [%s] to ::= [000000]\n", state.userId.c_str());
+    cube->color = "000000";
+  }
+}
+
+/**
+ * Update the NeoPixels.
+ */
+void setLEDColors() {
+  String colorString = "000000,000000,000000,000000,";
+  for (NeoCube* cube : neoCubes) {
+    for (uint8_t i = 0; i < 9; i++) {
+      colorString += cube->color + ",";
+    }
+  }
+
+  colorString = colorString.substring(0, colorString.length() - 1);
+  Serial.println(colorString);
+  strip.setMode(leds::AsyncNeoPixelMode::STATIC, colorString);
+}
 
 /**
  * Registers a callback in the circuit client to get informed about new
@@ -43,29 +79,51 @@ std::vector<String> userIds;
 void registerPresenceStateChangedCallback() {
   circuit->onUserUserPresenceChangedEvent([](Circuit::PresenceState state) {
     Serial.printf("Presence state of user ::= [%s] is ::= [%s]", state.userId.c_str(), state.presenceState.c_str());
+    for (NeoCube* cube : neoCubes) {
+      if (state.userId == cube->userId) {
+        updateNeoCube(cube, state);
+      }
+    }
+    setLEDColors();
   });
 }
 
 void setup() {
-    CommonUtilities::Utilities::setupSerial("--            Presence Board         --");
-    CommonUtilities::Utilities::connectToWifi(WIFI_NAME, WIFI_PASSWORT);
+  strip.setup();
+  strip.setMode(leds::AsyncNeoPixelMode::STATIC, "000000,000000,000000,000000,22FF22");
 
-    
-    userIds.push_back(NeoCube1.userId); 
-    userIds.push_back(NeoCube2.userId); 
-    userIds.push_back(NeoCube3.userId); 
-    userIds.push_back(NeoCube4.userId); 
-        
-    
-    circuit = new Circuit::CircuitClient(CIRCUIT_SYSTEM, 443, CIRCUIT_USER, CIRCUIT_PWD);
-    circuit->setup();
-    circuit->subscribeToPresence(userIds, [] (Circuit::ResultCode resultCode, std::vector<Circuit::PresenceState> states) {
+  CommonUtilities::Utilities::setupSerial("--            Presence Board         --");
+  CommonUtilities::Utilities::connectToWifi(WIFI_NAME, WIFI_PASSWORT);
 
-    });
-    registerPresenceStateChangedCallback();
+  userIds.push_back(c1->userId);
+  userIds.push_back(c2->userId);
+  userIds.push_back(c3->userId);
+  userIds.push_back(c4->userId);
+
+  neoCubes.push_back(c1);
+  neoCubes.push_back(c2);
+  neoCubes.push_back(c3);
+  neoCubes.push_back(c4);
+
+  circuit = new Circuit::CircuitClient(CIRCUIT_SYSTEM, 443, CIRCUIT_USER, CIRCUIT_PWD);
+  circuit->setup();
+  circuit->subscribeToPresence(userIds, [](Circuit::ResultCode resultCode, std::vector<Circuit::PresenceState> states) {
+    for (Circuit::PresenceState state : states) {
+      Serial.printf("Presence state of user ::= [%s] is ::= [%s]\n", state.userId.c_str(), state.presenceState.c_str());
+      for (NeoCube* cube : neoCubes) {
+        if (state.userId == cube->userId) {
+          updateNeoCube(cube, state);
+        }
+      }
+    }
+    setLEDColors();
+  });
+
+  registerPresenceStateChangedCallback();
 }
 
 void loop() {
   circuit->loop();
+  strip.loop();
+  //  server.loop();
 }
-
